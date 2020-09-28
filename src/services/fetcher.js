84baf63b6e429e459e5f3f8ecaf10ce13efcee16
Vladislav Rzheutskij <v.rzheutskij@hqsoftwarelab.com>
2020-09-28 16:46:28 +0300

    fix env variable

--- src/services/fetcher.js
+++ src/services/fetcher.js
@@ -1,7 +1,8 @@
 const fetcher = (url, settings) => {
   let requestUrl = `http://localhost:3000${url}`;
+  console.log(process)
   if (process.env.VERCEL_URL) {
-    requestUrl = `VERCEL_URL${url}`
+    requestUrl = `${process.env.VERCEL_URL}${url}`
   }
   return fetch(requestUrl, settings).then(res => res.json())
 }
